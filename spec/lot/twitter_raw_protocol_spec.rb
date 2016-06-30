0f683f8dccd425942d16c9def6b7a57ec9feac7d
mizhal <mizhal@gmail.com>
2016-06-30 05:57:42 +0000

    spec interface

--- spec/lot/twitter_raw_protocol_spec.rb
+++ spec/lot/twitter_raw_protocol_spec.rb
@@ -8,7 +8,8 @@ module Lot
   module DocumentCatalog
     describe Lot::DocumentCatalog::TwitterRawProtocol do
       it "provides a catalog that verifies interface 'TwitterRawProtocol' from architectural design" do
-        
+        ## this is really overkill for this simple gem, but insteresting as good practice
+
         catalog = TwitterRawProtocol.catalog
 
         ## exposes each one of the required classes 
@@ -34,8 +35,20 @@ module Lot
         ## END: Each class is a collection
         
         ## Each object is a persistable / activerecord pattern
-        ## END: Each object is a persistable / activerecord pattern
+        persistable_methods = [:save, :update, :destroy]
 
+        req_classes.each do |r_class|
+
+          _class = catalog.send(r_class)
+
+          obj = _class.new
+
+          persistable_methods.each do |c_meth|
+            expect {   obj.method(c_meth)  }.not_to raise_error(NameError)
+          end
+
+        end
+        ## END: Each object is a persistable / activerecord pattern
 
       end
     end
